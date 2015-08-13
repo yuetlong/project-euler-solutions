065ca12938e74aa5db1661d465ea813bfdb98c5d
yuetlong <yuetlong@icloud.com>
2015-08-13 14:00:15 -0400

    comment p047.clj

--- p047/p047.clj
+++ p047/p047.clj
@@ -8,13 +8,19 @@
                       (zero? (mod x f)) (recur (/ x f) (conj coll f) p)
                       :else (recur x coll r))))))
 
-(defn number-of-prime-factors [n]
-  )
 (defn p047
   "Evaluates the first of four consecutive integers having four distinct prime factors."
   [] (loop [s 2 c 0 l (lazy-primes)]
+                                        ;s contains the current number that is being checked
+                                        ;c is the counter
+                                        ;l helps taking out test cases where prime is in range
        (let [coll (prime-factors s)]
          (cond (= c 4) (- s 4)
+                                        ;if the counter reaches four, return answer
                (< (first l) (+ s (- 3 c))) (recur (inc (first l)) 0 (rest l))
+                                        ;if one of the numbers we'll check is prime, reset counter, start from prime + 1
                (= 4 (count coll)) (recur (inc s) (inc c) l)
-               :else (recur (inc s) 0 l)))))
+                                        ;if there are four distinct prime factors for s, check the next number, increment counter
+               :else (recur (inc s) 0 l)
+                                        ;else, s doesn't have four distinct primes, reset counter, move on to the next one
+               ))))
