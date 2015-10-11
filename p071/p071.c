8656044d24dcb74e562061583c8601f946d16465
YuetLong Leung <yuetlong@icloud.com>
2015-10-11 16:45:26 +0000

    added p071

--- p071/p071.c
+++ p071/p071.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
+#include <math.h>
 
 int gcd_iter (int u, int v){
-  int t;
-  while (v){
-    t = u;
-    u = v;
-    v = t % v;
-  }
-  return u < 0 ? -u : u;
+	int t;
+	while (v){
+		t = u;
+		u = v;
+		v = t % v;
+	}
+	return u < 0 ? -u : u;
 }
 
 int main(){
-  int d = 1000000;
-  printf("%d\n", (int) ((float) d) / 7 * 3);
-  while(gcd_iter(d, (int) ((float) d) / 7 * 3) != 1){
-      d--;
-    }
-  printf("%d\n",d);
+	double answer;
+	double max_val = 0;
+	double d       = 1000000.0f;
+	double n;
+	
+	do{
+		n = floor(d * 3 / 7);
+
+		while ( gcd_iter (n, d) != 1){
+			n--;
+		}
+
+		if( (n/d) > max_val){
+			max_val = n/d;
+			answer = n;
+		}
+	}
+	while (--d > 7);
+	printf("%.0f \n", answer);
 }
