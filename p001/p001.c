8b1e4cf769c909167ff503320a18f32aee802def
yuetlong <yuetlong@icloud.com>
2015-10-17 00:01:58 -0400

    updated p001,p037

--- p001/p001.c
+++ p001/p001.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-  int s3 = 0, s5 = 0, s15 = 0;
-  int i;
+  int a = 0;
 
-  for (i = 0; i < 1000; i++) {
-    if (i % 3 == 0) {
-      s3 += i;
-    }
-    if (i % 5 == 0) {
-      s5 += i;
-    }
-    if (i % 15 == 0) {
-      s15 += i;
-    }
-  }
-  printf("%d\n", s3 + s5 - s15);
+  for (int i = 0; i < 1000; i++) 
+    if (i % 3 == 0 || i % 5 == 0)
+      a += i;
 
-  return 0;
+
+  printf("%d\n", a);
 }
--- p037/p037.py
+++ p037/p037.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
-
-from sieve import Sieve
+from miller_rabin import is_probable_prime
 
 def truncate(n):
-    return list( int(str(n)[i:]) for i in range( len(str(n)) )) + list( int(str(n)[:i]) for i in range(1, len(str(n)) ))
+    l1 = list( int( str(n)[i:]) for i in range(   len(str(n))))
+    l2 = list( int( str(n)[:i]) for i in range(1, len(str(n))))
+    return l1 + l2
 
-si = Sieve(1000000)
-answer = sum (p for p in si.getAllPrimes()[4:] if all(map(si.isPrime, truncate(p))))
-print(answer)
+def sieve(n):
+    multiples = set()
+    for i in range(2, n+1):
+        if i not in multiples:
+            yield i
+            multiples.update(range(i*i, n+1, i))
+
+si = sieve(1000000)
+answer = sum (p for p in si if all ( map (is_probable_prime, truncate(p))))
+print(answer - 2 - 3 - 5 - 7)
