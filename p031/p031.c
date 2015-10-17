e65fe883aa5a35c146846700977dfdc0f923a986
yuetlong <yuetlong@icloud.com>
2015-10-17 01:08:54 -0400

    reindent p031

--- p031/p031.c
+++ p031/p031.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
+int main(){
   int i,j;
   int coinSizes[8] = { 1, 2, 5, 10, 20, 50, 100, 200 };
   int *ways;
 
   ways = (int*) calloc(201, sizeof(int));
-  
-
   ways[0] = 1;
 
-  for (i = 0; i < 8; i++) {
-    for (j = coinSizes[i]; j < 201; j++) {
+  for (i = 0; i < 8; i++) 
+    for (j = coinSizes[i]; j < 201; j++) 
       ways[j] += ways[j - coinSizes[i]];
-    }
-  }
 
   printf("%d\n", ways[200]);
   free(ways);
-  return 0;
 }
